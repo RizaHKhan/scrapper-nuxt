b0dc1592db009e103c34473b71d0d567d8583692
RizaHKhan <khanriza@gmail.com>
2020-03-21 22:06:57 -0400

    Final Landing Styling Commit

--- components/landing/Landing.vue
+++ components/landing/Landing.vue
@@ -7,17 +7,19 @@
       </div>
     </div>
     <div class="landing__log">
-      <form class="landing__log__form">
-        <label for="log">Login</label>
-        <input id="login" type="radio" name="log" value="login" checked />
-        <label for="log">Register</label>
-        <input id="register" type="radio" name="log" value="register" />
-        <label for="email">Email</label>
-        <input name="email" type="email" />
-        <label for="password">Password</label>
-        <input name="password" type="password" />
-        <input type="submit" value="Submit" class="button--danger" />
-      </form>
+      <div class="landing__log__container">
+        <form class="landing__log__container__form">
+          <label for="log">Login</label>
+          <input id="login" type="radio" name="log" value="login" checked />
+          <label for="log">Register</label>
+          <input id="register" type="radio" name="log" value="register" />
+          <label for="email">Email</label>
+          <input name="email" type="email" />
+          <label for="password">Password</label>
+          <input name="password" type="password" />
+          <input type="submit" value="Submit" class="button--danger" />
+        </form>
+      </div>
     </div>
   </div>
 </template>
